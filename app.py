45f3bb78aa20a4526f98a6f6c9187763534b4ca2
Andrew <63203237+Andrew-s-web@users.noreply.github.com>
2024-11-20 17:31:54 +0500

    Добавлены уведомления о смене статуса заявки и реализован фильтр заявок по номеру

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from doctest import master
 from typing import Annotated, Optional
 from fastapi import FastAPI, Form
 import datetime
@@ -31,22 +30,47 @@ repo = [
         description = 'Raspberry Pi fails',
         client = 'Raspberry',
         status = 'Running',
+    ),
+    OrderItem(
+        number = 2,
+        startDate = datetime.date(2020, 1, 1),
+        device = 'Raspberry Pi',
+        problemType = 'CPU',
+        description = 'Raspberry Pi fails',
+        client = 'Raspberry',
+        status = 'Running',
+    ),
+    OrderItem(
+        number = 3,
+        startDate = datetime.date(2020, 1, 1),
+        device = 'Raspberry Pi',
+        problemType = 'CPU',
+        description = 'Raspberry Pi fails',
+        client = 'Raspberry',
+        status = 'Running',
     )
 ]
 app = FastAPI()
 
+message = ''
+
 @app.get("/orders")
-def get_orders():
-    return repo
+def get_orders(param=None):
+    global message
+    if param:
+        return {'repo': [o for o in repo if o.number == int(param)], "message": message}
+    return {'repo': repo, "message": message}
 @app.post("/orders")
 def create_order(dto : Annotated[OrderItem, Form()]):
     repo.append(dto)
 @app.post("/update")
 def update_order(dto : Annotated[UpdateOrderDTO, Form()]):
+    global message
     for o in repo:
         if o.number == dto.number:
             if dto.status != o.status and dto.status != "":
                 o.status = dto.status
+                message += f"Статус заявки №{o.number} изменен"
             if dto.description != "":
                 o.description = dto.description
             if dto.master != "":
